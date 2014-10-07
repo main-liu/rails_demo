8a7c3aa126afd020c16a58756840fa8bed503e26
Ming <ming@ming-VirtualBox.(none)>
2014-10-07 12:13:54 +0800

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,10 @@ uniqueness: { case_sensitive: false }
   validates :password_confirmation, presence: true
 
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
+
+  private
+    def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
 end
